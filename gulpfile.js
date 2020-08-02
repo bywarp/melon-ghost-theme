f5c1a0ae0148bfd940c601d0b57c9fbc599875c9
Mike M <mikester70387@gmail.com>
2020-08-02 01:30:23 -0400

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+
+const DEST = 'assets/';
+
+// build the css assets
+gulp.task('css', function() {
+    return gulp.src([
+        'node_modules/bootstrap/dist/css/bootstrap.min.css',
+        'node_modules/bootstrap/dist/css/bootstrap.min.css.map',
+        'node_modules/animate.css/animate.min.css'
+      ])
+      .pipe(concat('build.css'))
+      .pipe(gulp.dest(DEST+'/css'))
+      .pipe(browserSync.stream());
+});
+
+// build the js assets
+gulp.task('scripts', function() {
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.min.js',      
+        'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        'node_modules/@fortawesome/fontawesome-free/js/all.min.js',
+        'node_modules/wowjs/dist/wow.min.js',
+        'node_modules/jquery.easing/jquery.easing.min.js',
+        'assets/js/jquery.fitvids.js'
+      ])
+      .pipe(concat('build.js'))
+      .pipe(gulp.dest(DEST+'/js'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())
+      .pipe(gulp.dest(DEST+'/js'))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('build', gulp.parallel('css', 'scripts'));
