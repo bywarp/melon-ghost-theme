d5f5a54076654fbf32cc9abcd8a77fd7b03cbbc2
Mike M <mikester70387@gmail.com>
2020-08-14 03:37:20 -0400

    Finished up the post page (except for the related posts section), altered the home & all posts pages, and changed some various things elsewhere.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,81 @@
 // This file has the ghost-bootstrap theme javascript
 $(document).ready(function() {
 
+    // Collapse navigation
+    $('.navbar-main .collapse').on('hide.bs.collapse', function () {
+        var $this = $(this);
+        $this.addClass('collapsing-out');
+    });
+
+    $('.navbar-main .collapse').on('hidden.bs.collapse', function () {
+        var $this = $(this);
+        $this.removeClass('collapsing-out');
+    });
+
+    $('.navbar-main .dropdown').on('hide.bs.dropdown', function () {
+        var $this = $(this).find('.dropdown-menu');
+
+        $this.addClass('close');
+
+        setTimeout(function(){
+            $this.removeClass('close');
+        }, 200);
+
+    });
+
     // remove preloader
     $('.preloader').fadeOut('slow');
     
-    // load tooltips
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
+    // Datepicker
+    $('.datepicker')[0] && $('.datepicker').each(function() {
+        $('.datepicker').datepicker({
+            disableTouchKeyboard: true,
+            autoclose: false
+        });
     });
-    // load popovers
-    $(function () {
-        $('[data-toggle="popover"]').popover()
-    })
+
+    // Tooltip
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // Popover
+    $('[data-toggle="popover"]').each(function() {
+        var popoverClass = '';
+        if($(this).data('color')) {
+            popoverClass = 'popover-'+$(this).data('color');
+        }
+        $(this).popover({
+            trigger: 'focus',
+            template: '<div class="popover '+ popoverClass +'" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+        })
+    });
+
+    // Additional .focus class on form-groups
+    $('.form-control').on('focus blur', function(e) {
+        $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
+    }).trigger('blur');    
+
+    // When in viewport
+    $('[data-toggle="on-screen"]')[0] && $('[data-toggle="on-screen"]').onScreen({
+        container: window,
+        direction: 'vertical',
+        doIn: function() {
+            //alert();
+        },
+        doOut: function() {
+            // Do something to the matched elements as they get off scren
+        },
+        tolerance: 200,
+        throttle: 50,
+        toggleClass: 'on-screen',
+        debug: false
+    });
 });
+
+Logging = {
+    info: (module, body) => {
+        console.log(`%c ${module} %c ${body}`, "background-color: #66A7C5; color: #eee;", "color: #eee;");
+    },
+    warn: (module, body) => {
+        console.log(`%c ${module} %c ${body}`, "background-color: #f44336; color: #eee;", "color: #eee;");
+    }
+}
